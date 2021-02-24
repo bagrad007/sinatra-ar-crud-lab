80dd54364aaae3701411fed85fbda88f7eb41765
Luke Gill <bagrad007@gmail.com>
2021-02-24 14:14:50 -0500

    commit tree 97c01966495ac081dfb4df3b0dfa1386b0fa0a4b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,49 @@
-
-require_relative '../../config/environment'
+require_relative "../../config/environment"
 
 class ApplicationController < Sinatra::Base
-
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
   end
 
-  get '/' do
+  get "/" do
+  end
+
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+
+  post "/articles" do
+    create_article = Article.create({ title: params[:title], content: params[:content] })
+    # binding.pry
+    redirect "articles/#{create_article.id}"
+  end
+
+  get "/articles/new" do
+    erb :new
+  end
+
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update({ title: params[:title], content: params[:content] })
+    redirect "articles/#{@article.id}"
+  end
+
+  delete "/articles/:id" do
+    # binding.pry
+    article = Article.find(params[:id])
+    article.delete
+    redirect "/articles"
   end
 end
